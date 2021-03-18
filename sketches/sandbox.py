f63b80f8bab0c6ed431137d6995b00d347aea0c6
Kyle Stewart <kylestew@gmail.com>
2021-03-17 22:40:12 -0700

    Upgrade sketch module

--- sketches/sandbox.py
+++ sketches/sandbox.py
@@ -1,25 +1,28 @@
 #%%
-# from .sketch import Sketch
-from sketch import Sketch
+from .sketch import Sketch
+
+# from sketch import Sketch
 
 
 class Sandbox(Sketch):
     def __init__(self, w, h):
         back = [0, 0, 0, 0]
         front = [1, 0, 1]
-        super().__init__(w, h, back, front)
+        range = [-1, 1]
+        super().__init__(w, h, range, back, front)
 
     def draw(self):
-        # self.ctx.set_front([0, 1, 1])
-        self.ctx.rectangle(0, 0, 200, 200)
-        self.ctx.fill()
-        # self.ctx.set_front([0, 1, 0, 1])
-        # render.circle((0.5, 0.5), 0.25, fill=True)
+        # self.set_front([1, 0, 1])
+        self.circle((0, 0), self.parmA, fill=True)
+        # self.circle((-1., -1.), 0.25, fill=True)
+        # self.circle((1., 1.), 0.25, fill=True)
+        # self.line((-1, 1), (1, -1))
+        # self.set_line_width(4)
+        # self.line((0, -1), (-1, 1))
 
 
-%load_ext ipython_cairo
-
-sketch = Sandbox(1920, 1080)
-d = sketch.render()
-
-sketch.ctx
+# %load_ext ipython_cairo
+# sketch = Sandbox(1920, 1080)
+# sketch.set_params(0.85, 0.25)
+# d = sketch.render()
+# sketch.ctx
--- sketches/sketch.py
+++ sketches/sketch.py
@@ -1,27 +1,15 @@
 import cairo
+from numpy import pi
 
 
 class Sketch:
-    def __init__(self, w, h, back, front):
-        print("init", w, h, back, front)
-
+    def __init__(self, w, h, range, back, front):
         self.w = w
         self.h = h
         self.back = back
         self.front = front
+        self.range = range
 
-        # size of one pixel
-        # self.pix = 1.0
-
-        self.__init_cairo()
-
-    def update_canvas(self, w, h):
-        print("update_canvas", w, h)
-
-        if self.w == w and self.h == h:
-            return
-        self.w = w
-        self.h = h
         self.__init_cairo()
 
     def __init_cairo(self):
@@ -30,6 +18,32 @@ class Sketch:
 
         self.sur = sur
         self.ctx = ctx
+
+        # scale cairo to fit range
+        # crop to fit (not fill)
+        d0, d1 = self.range
+        small_side = 0
+        xoff = 0
+        yoff = 0
+        if self.w > self.h:
+            small_side = self.h
+            xoff = (self.w - self.h) / 2
+        else:
+            small_side = self.w
+            yoff = (self.h - self.w) / 2
+
+        xscale = small_side / (d1 - d0)
+        yscale = small_side / (d1 - d0)
+        print(self.w, self.h, xscale, yscale, xoff, yoff)
+
+        # ctx.translate(-d0 * xscale - xoff, -d0 * yscale - yoff)
+        ctx.translate(xoff, -yoff)
+        ctx.scale(xscale, -yscale)
+        ctx.translate(-d0, -d1)
+
+        # update line width
+        self.one = 1.0 / xscale
+        ctx.set_line_width(self.one)
 
         self.__clear_canvas()
 
@@ -41,22 +55,80 @@ class Sketch:
         ctx.fill()
         ctx.set_source_rgba(*self.front)
 
+    def update_canvas(self, w, h):
+        if self.w == w and self.h == h:
+            return
+        self.w = w
+        self.h = h
+        self.__init_cairo()
+
+    def set_params(self, parmA, parmB):
+        self.parmA = parmA
+        self.parmB = parmB
+
     def draw(self):
         # TODO: implement in child
         pass
 
     def render(self):
         self.__clear_canvas()
-
-        # TODO: scale canvas
         self.ctx.save()
-        # self.domain = [0, self.w]
-        # self.range = [0, self.h]
-        # self.remap([0, 1], [0, 1])
-
         self.draw()
-
-        # restore
         self.ctx.restore()
-
         return self.sur.get_data()
+
+    def set_front(self, c):
+        self.front = c
+        self.ctx.set_source_rgba(*c)
+
+    def set_back(self, c):
+        self.back = c
+
+    def set_operator(self, op):
+        self.ctx.set_operator(op)
+
+    def set_line_width(self, width):
+        self.ctx.set_line_width(self.one * width)
+
+    def line(self, a, b):
+        ctx = self.ctx
+        ctx.move_to(a[0], a[1])
+        ctx.line_to(b[0], b[1])
+        ctx.stroke()
+
+    def path(self, xy, closed=False, fill=False):
+        ctx = self.ctx
+        ctx.move_to(*xy[0, :])
+        for p in xy:
+            ctx.line_to(*p)
+        if closed:
+            ctx.close_path()
+        if fill:
+            ctx.fill()
+        else:
+            ctx.stroke()
+
+    def circle(self, xy, r, fill=False):
+        """
+        xy : (float, float)
+            point (x, y)
+        r : float
+            radius of circle
+        fill : bool, optional
+            stroke or fill circle (default stroke)
+        """
+        ctx = self.ctx
+        x, y = xy
+        ctx.arc(x, y, r, 0, pi * 2)
+        if fill:
+            ctx.fill()
+        else:
+            ctx.stroke()
+
+    def rect(self, x, y, w, h, fill=False):
+        ctx = self.ctx
+        ctx.rectangle(x, y, w, h)
+        if fill:
+            ctx.fill()
+        else:
+            ctx.stroke()
